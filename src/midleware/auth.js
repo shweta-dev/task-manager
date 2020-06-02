dd3aa5d4c041d4411e8896431fd4caf28484dfd1
shweta agarwal <shweta.agarwal@and.digital>
2020-06-02 11:24:05 +0100

    Eslint and preetier added

--- src/midleware/auth.js
+++ src/midleware/auth.js
@@ -1,24 +1,24 @@
-const jwt = require('jsonwebtoken')
-const users = require('../model/users.js')
+const jwt = require('jsonwebtoken');
+const users = require('../model/users.js');
 
-const auth = async (req,res,next)=> {
-   try{
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
+    const user = await users.findOne({
+      _id: decoded.id,
+      'tokens.token': token
+    });
 
-    const token = req.header('Authorization').replace('Bearer ','')
-    const decoded = jwt.verify(token, process.env.TOKEN_SECRET_KEY)
-    const user = await users.findOne({ _id :decoded.id , 'tokens.token': token})
-    
-    if(!user){
-        throw new Error()
+    if (!user) {
+      throw new Error();
     }
-     req.user = user
-     req.token = token
-    next()
-   }
-   catch(e){
-    res.status(401).send({error: 'Please authenticate'})
-   }
-   
-}
+    req.user = user;
+    req.token = token;
+    next();
+  } catch (e) {
+    res.status(401).send({ error: 'Please authenticate' });
+  }
+};
 
-module.exports = auth
+module.exports = auth;
